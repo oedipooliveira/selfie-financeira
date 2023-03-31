90564d4cea9f9f410ec6c37b3a920c6a6f53d865
EdiCursos <edicursosprofessor@gmail.com>
2023-03-31 20:55:26 -0300

    Adicionando evento aos inputs

--- src/componentes/FormularioGrupoConta/FormularioGrupoConta.js
+++ src/componentes/FormularioGrupoConta/FormularioGrupoConta.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './FormularioGrupoConta.css';
 import Formulario from '../Formulario/Formulario';
 import CampoTexto from '../CampoTexto/CampoTexto';
@@ -22,14 +23,34 @@ const FormularioGrupoConta = () => {
 
     const aoSalvar = (event) => {
         event.preventDefault();
-        console.log('Form submetido');
+        console.log('Form submetido => ', nome, descricao, grupo);
     }
+
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [grupo, setGrupo] = useState('');
 
     return (
         <Formulario titulo="Grupo de contas" onSubmit={aoSalvar}>
-            <CampoTexto label="Nome" obrigatorio={true} placeholder="Digite o nome do grupo"></CampoTexto>
-            <CampoTexto label="Descrição" placeholder="Digite a descrição do grupo"></CampoTexto>
-            <ListaSuspensa label="Grupo" itens={grupos}></ListaSuspensa>
+            <CampoTexto
+                valor={nome}
+                aoAlterado={valor => setNome(valor)}
+                label="Nome"
+                obrigatorio={true}
+                placeholder="Digite o nome do grupo"
+            />
+            <CampoTexto
+                valor={descricao}
+                aoAlterado={valor => setDescricao(valor)}
+                label="Descrição"
+                placeholder="Digite a descrição do grupo"
+            />
+            <ListaSuspensa
+                valor={grupo}
+                aoAlterado={valor => setGrupo(valor)}
+                label="Grupo"
+                itens={grupos}
+            />
             <Botao>
                 Salvar
             </Botao>
--- src/componentes/ListaSuspensa/ListaSuspensa.js
+++ src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -1,10 +1,14 @@
 import './ListaSuspensa.css';
 
 const ListaSuspensa = (props) => {
+    const aoDigitado = (event) => {
+        props.aoAlterado(event.target.value);
+    }
+
     return (
         <div className="lista-suspensa">
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>
+            <select value={props.valor} onChange={aoDigitado} required={props.obrigatorio}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
