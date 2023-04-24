a2dc702460c8d14a12d54a8822530d055a366f36
edipojoseoliveira <edipojoseoliveira@gmail.com>
2023-04-24 20:46:20 -0300

    Cadastro de grupos

--- src/componentes/FormularioGrupo/FormularioGrupo.js
+++ src/componentes/FormularioGrupo/FormularioGrupo.js
@@ -1,19 +1,55 @@
-import './FormularioGrupoConta.css';
-import { useState } from 'react';
+import './FormularioGrupo.css';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 import Formulario from '../Formulario/Formulario';
 import CampoTexto from '../CampoTexto/CampoTexto';
 import Botao from '../Botao/Botao';
 
 const FormularioGrupo = (props) => {
 
-    const aoSalvar = (event) => {
-        event.preventDefault();
-        props.aoSalvar({
-            nome
-        });
-    }
+    const { id } = useParams();
+    const navigate = useNavigate();
 
     const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        async function buscarGrupo() {
+            const response = await fetch(`http://localhost:8080/grupos/${id}`);
+            const grupoJson = await response.json();
+            setNome(grupoJson.nome);
+        }
+
+        if (id) {
+            buscarGrupo();
+        }
+    }, [id]);
+
+    const aoSalvar = (event) => {
+        event.preventDefault();
+
+        const requestOptions = {
+            method: (id != null ? "PUT" : "POST"),
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(
+                {
+                    nome
+                }
+            )
+        };
+
+        fetch(`http://localhost:8080/grupos/${id != null ? id : ''}`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                navigate('/grupo');
+            });
+
+        setNome('');
+    }
+
+    const aoCancelar = () => {
+        navigate('/grupo');
+    }
 
     return (
         <Formulario titulo="Grupos" onSubmit={aoSalvar}>
@@ -24,9 +60,14 @@ const FormularioGrupo = (props) => {
                 obrigatorio={true}
                 placeholder="Digite o nome do grupo"
             />
-            <Botao>
-                Salvar
-            </Botao>
+            <div className="flexbox">
+                <Botao aoClicar={aoCancelar} type="button" cor="blue">
+                    <FaArrowLeft size={15} />
+                </Botao>
+                <Botao type="submit" largura="full">
+                    Salvar
+                </Botao>
+            </div>
         </Formulario>
     );
 }
